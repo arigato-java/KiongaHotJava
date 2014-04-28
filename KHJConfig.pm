6a273b328c9904864371f55dfcb0c97a23d849b0
Hisanobu Tomari <htomari@myopera.com>
2014-04-28 20:59:58 +0900

    Initial commit

---
+++ KHJConfig.pm
@@ -0,0 +1,20 @@
+# -*- encoding:utf-8 -*-
+use strict;
+use warnings;
+use utf8;
+package KHJConfig;
+
+# Set location ID. http://weather.livedoor.com/forecast/rss/primary_area.xml
+# 130010 for Tokyo, 140020 for Odawara, 220030 for Mishima
+our $locationParam="?city=130010";
+
+# Say HotJava if temp is equal to or greater than ...
+our $HJthresh=28; # degrees celsius
+
+# Constant
+our $livedoorWeatherURL="http://weather.livedoor.com/forecast/webservice/json/v1";
+our $targetDateLabel="今日";
+#our $targetDateLabel="明日";
+our $hotjavaMessage="今日も気温がHotJava "; # Weather URL is appended to this string
+
+1;
---
+++ KionHotJava.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl -CS
+use strict;
+use warnings;
+use utf8;
+use HTTP::Tiny;
+use JSON;
+use KHJConfig;
+
+sub fetch_temp {
+  my $response=HTTP::Tiny->new->get($KHJConfig::livedoorWeatherURL.$KHJConfig::locationParam);
+  die "Accessing weather information failed.\n" unless $response->{success};
+  die "Response length is 0.\n" if !length $response->{content};
+  my $weather=decode_json($response->{content});
+  my $forecasts=${$weather}{forecasts};
+  my $maxC=undef;
+  foreach(@$forecasts) {
+    if(${$_}{dateLabel} eq $KHJConfig::targetDateLabel) {
+      my $temps=${$_}{temperature};
+      my $maxtemps=${$temps}{max};
+      if(defined($maxtemps)) {
+	$maxC=${$maxtemps}{celsius};
+      }
+    }
+  }
+  if(defined($maxC)) {
+    my %result;
+    $result{temperature}=$maxC;
+    $result{link}=${$weather}{link};
+    return \%result;
+  } else {
+    die "Max temperature not in the forecast -- maybe you need to run this program earlier in the morning.\n";
+  }
+}
+
+sub run_tempHotJava {
+  my $temp=fetch_temp();
+  if(${$temp}{temperature} gt $KHJConfig::HJthresh) {
+    # Do the HotJava thing!
+    print $KHJConfig::hotjavaMessage.${$temp}{link}."\n";
+  }
+}
+
+run_tempHotJava();
+
